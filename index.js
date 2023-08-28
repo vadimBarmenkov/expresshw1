0b49a4eeeab8f91c44d0a5c21006cc6c81b0fccf
Vadim Barmenkov <vadimbarmenkov@gmail.com>
2023-08-28 10:53:12 +0300

    commit 1

--- index.js
+++ index.js
@@ -37,19 +37,53 @@ app.get('/api/books/:id', (req, res) => {
     }
 });
 app.post('/api/books', (req, res) => {
+    const {books} = store;
+    const {title, desc, author, favorite, fileCover, fileName} = req.body;
+
+    const newBook = new Book(books.length , title, desc, author, favorite, fileCover, fileName);
+    books.push(newBook);
+
+    res.status(201);
+    res.json(newBook);
     
 });
 app.post('/api/user/login', (req, res) => {
+    const testUser = { id: 1, mail: "test@mail.ru" };
+
+    res.status(201);
+    res.json(testUser);
 
 });
 app.put('/api/books/:id', (req, res) => {
+    const {books} = store;
+    const {title, desc} = req.body;
+    const {id} = req.params;
+
+    const idx = books.findIndex(el => el.id === id);
+
+    if (idx !== -1){
+        books[idx] = {...books[idx], title, desc};
+        res.json(books[idx]);
+
+    }else {
+        res.status(404)
+    }
 
 });
 app.delete('/api/books/:id', (req, res) => {
+    const {books} = store;
+    const {id} = req.params;
+
+    const idx = books.findIndex(el => el.id === id);
+
+    if (idx !== -1){
+        books.splice(idx, 1)
+
+    }else {
+        res.status(404)
+    }
 
 });
-
-
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
